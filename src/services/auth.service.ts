7a11354157f3c24b5351917951466dd794f2175d
LeonardG1993 <leonardguilloryiv@gmail.com>
2022-09-20 14:51:38 -0500

    Added some services

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { url } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { User } from 'src/models/user';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  url: string = url;
+  httpOptions = {
+    headers: new HttpHeaders({'Content-Type' : 'application/json'})
+  };
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login (username: string, password: string): Observable<any> {
+    const payload = {username, password}
+
+    return this.http.post<any>(url + '/login', payload, { observe: 'response' })
+  }
+
+  logout(): void{
+    this.http.post(`${this.url}/logout`, null);
+  }
+
+
+
+
+
+
 }
