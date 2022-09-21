3359a1fea8b6adb0e02f7caf228fb8a92888e864
LeonardG1993 <leonardguilloryiv@gmail.com>
2022-09-21 14:38:57 -0500

    Added service methods

--- src/services/predict.service.ts
+++ src/services/predict.service.ts
@@ -1,9 +1,49 @@
+import { Prediction } from './../models/prediction';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { url } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PredictService {
 
-  constructor() { }
+  predictionUrl: string = url + `/prediction`;
+
+  httpOptions = {
+    headers: new HttpHeaders ({'Content-Type' : 'application/json'})
+  }
+
+  constructor(private http: HttpClient) { }
+
+  findAllPredictions(): Observable<Prediction[]>{
+    return this.http.get<Prediction[]>(this.predictionUrl, this.httpOptions)
+      .pipe(catchError(this.handleError));
+  }
+
+  findPredictionById(id: number): Observable<Prediction>{
+    return this.http.get<Prediction>(`${this.predictionUrl}/${id}`, this.httpOptions)
+     .pipe(catchError(this.handleError));
+  }
+
+  uploadPrediction(prediction: any): Observable<Prediction> {
+    return this.http.post<Prediction>(`${this.predictionUrl}`, prediction, this.httpOptions)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(httpError: HttpErrorResponse){
+    
+    if (httpError.error instanceof ErrorEvent){
+      console.log('An error has occured: ', httpError.error.message);
+    } else {
+      console.error(`
+      Backend returned code ${httpError.status}
+      with body: ${httpError.error}`)
+    }
+
+    return throwError(() => new Error('something went wrong'));
+  }
+
+
 }
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,9 +1,54 @@
+import { Prediction } from './../models/prediction';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { url } from 'src/environments/environment';
+import { User } from 'src/models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  userUrl: string = url + `/user`;
+  predictionUrl: string = url + `/prediction/user`;
+
+  httpOptions = {
+    headers: new HttpHeaders ({'Content-Type' : 'application/json'})
+  }
+
+  constructor(private http: HttpClient) { }
+
+  findUserByUsername(username: string): Observable<User>{
+    return this.http.get<User>(`${this.userUrl}/${username}`, this.httpOptions)
+     .pipe(catchError(this.handleError));
+  }
+
+  findUserById(id: number): Observable<User>{
+    return this.http.get<User>(`${this.userUrl}/${id}`, this.httpOptions)
+     .pipe(catchError(this.handleError));
+  }
+
+  registerUser(user: User): Observable<User> {
+    return this.http.post<User>(`${this.userUrl}`, user, this.httpOptions)
+      .pipe(catchError(this.handleError));
+  }
+
+  getMyPredictions(id: number): Observable<Prediction[]>{
+    return this.http.get<Prediction[]>(`${this.predictionUrl}/${id}`, this.httpOptions)
+     .pipe(catchError(this.handleError));
+  }
+
+  private handleError(httpError: HttpErrorResponse){
+    
+    if (httpError.error instanceof ErrorEvent){
+      console.log('An error has occured: ', httpError.error.message);
+    } else {
+      console.error(`
+      Backend returned code ${httpError.status}
+      with body: ${httpError.error}`)
+    }
+
+    return throwError(() => new Error('something went wrong'));
+  }
 }
